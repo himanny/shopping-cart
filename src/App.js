dcf3a6ec88bd40ff3b2c5a71b8ad5a0340839b9f
himannys <himannys@gmail.com>
2021-03-09 20:21:27 +0530

    cart component added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Cart from "./components/Cart";
 import Filter from "./components/Filter";
 import Products from "./components/Products";
 import data from "./data.json"
@@ -9,31 +10,57 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
+      cartItems: [],
       size: "",
       sort: "",
     }
+  }
+
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter(x => x._id !== product._id),
+    });
+  }
+
+  addToCart = (product) => {
+    console.log(product);
+    debugger;
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+    this.setState({ cartItems });
   }
 
   sortProducts = (event) => {
     const sort = event.target.value;
     this.setState((state) => ({
       sort: sort,
-      products : this.state.products
-      .slice()
-      .sort((a,b) =>
-                sort === "lowest" 
-                ? a.price > b.price 
-                ? 1 
-                : -1  
-                : sort === "highest" 
-                ? a.price < b.price
-                ? 1 
-                : 
-                -1 
-                :a._id > b._id
-                ? 1 
-                : -1 
-      ),
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+              ? a.price < b.price
+                ? 1
+                :
+                -1
+              : a._id > b._id
+                ? 1
+                : -1
+        ),
     }));
   }
 
@@ -70,10 +97,14 @@ class App extends React.Component {
                 sortProducts={this.sortProducts}
               >
               </Filter>
-              <Products products={this.state.products}> </Products>
+              <Products products={this.state.products} addToCart={this.addToCart}> </Products>
             </div>
             <div className="sidebar">
-              CartItems
+              <Cart 
+              cartItems={this.state.cartItems}
+              removeFromCart={this.removeFromCart}
+              >
+              </Cart>
             </div>
           </div>
 
